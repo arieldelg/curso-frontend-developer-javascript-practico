f032c6dd2300e2d577355c4a2bb31c6354eeccd3
arieldelg <136664352+arieldelg@users.noreply.github.com>
2023-07-12 18:08:57 -0700

    https://github.com/arieldelg/curso-frontend-developer-javascript-practico.git

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuNavBar = document.querySelector('.menu')
+const menuHamburguesa = document.querySelector('.mobile-menu')
 
 const iniciar = () => {
     menuEmail.addEventListener('click', showOrHide)
+    menuNavBar.addEventListener('click', menuMobile)
+}
+
+const menuMobile = () => {
+    menuHamburguesa.classList.toggle('inactive');
 }
 
 const showOrHide = () => {
     desktopMenu.classList.toggle('inactive');
 }
-
-
 
 window.addEventListener('load', iniciar)
